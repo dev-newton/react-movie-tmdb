7fd209531f20a556339a6f671bfc0dc53469c426
newton imonjirie <newtonimonjirie@gmail.com>
2021-11-28 07:31:46 +0100

    created grid component to render 2 images per row

---
+++ src/components/TwoColGrid/TwoColGrid.jsx
@@ -0,0 +1,22 @@
+import "./TwoColGrid.css";
+
+const TwoColGrid = (props) => {
+  const renderComponents = () => {
+    const gridComponents = props.children.map((element, i) => {
+      return (
+        <div key={i} className="grid__element">
+          {element}
+        </div>
+      );
+    });
+    return gridComponents;
+  };
+
+  return (
+    <div className="grid">
+      <div className="grid__content">{renderComponents()}</div>
+    </div>
+  );
+};
+
+export default TwoColGrid;
